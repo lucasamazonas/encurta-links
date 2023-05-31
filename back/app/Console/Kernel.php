ec7ddda0790975762d9cf35b57c69390c7363ee6
Lucas Amazonas <lucassilvaamazonas@gmail.com>
2023-05-30 21:07:10 -0400

    criado cron para excluir clicks no primeiro dia de cada mes

--- back/app/Console/Kernel.php
+++ back/app/Console/Kernel.php
@@ -4,24 +4,22 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            DB::table('clicks')->delete();
+        })->cron('0 0 1 * *');
     }
 
     /**
      * Register the commands for the application.
-     *
-     * @return void
      */
     protected function commands()
     {
